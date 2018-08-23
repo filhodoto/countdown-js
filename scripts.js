44fd8dfd50b049e09e83c06456dee869bcd22e32
Gonçalo Ramalho <goncaloramalho@Goncalos-iMac-New.local>
2018-08-23 15:38:12 +0200

    Add manual minutes in form, css
    - Add option to input minutes in form
    - CSS changes
    - Add feedback with bg images

--- scripts.js
+++ scripts.js
@@ -1,9 +1,17 @@
 // Define countdown var so we can store a setInterval and later clear it
 let countdown;
 
-// Save DOM element where will display values
+// Save DOM elementsvalues
 const timeLeftDisplayer = document.querySelector('.display__time-left');
 const endTimeDisplayer = document.querySelector('.display__end-time');
+
+// Get all buttons
+const buttons = document.querySelectorAll('.timer__button');
+
+const gifUrl = {
+    beBack: 'https://media.giphy.com/media/JDKxRN0Bvmm2c/giphy.gif',
+    back: 'https://media.giphy.com/media/yj5UdA4elp8Wc/giphy.gif'
+};
 
 /**
  *  Format values to use extra '0' if value < 10
@@ -18,11 +26,20 @@ const formatVal = (val) => val < 10 ? `0${val}` : val;
  */
 function timer(seconds) {
 
+    // Clear previous timer
+    clearInterval(countdown);
+
     const now = Date.now(); // Get current time
     const after = now + seconds * 1000; // Get time and 1s now
 
     // Show time left
     displayTimeLeft(seconds);
+
+    // change gif url and show it
+    document.querySelector('.timer').style.backgroundImage = `url("${gifUrl.beBack}")`;
+
+    // Add overlay to display
+    document.querySelector('.display').classList.add('-overlay');
 
     // Show whe timer should end
     displayEndTime(after);
@@ -34,9 +51,14 @@ function timer(seconds) {
 
         // Stop interval if secondsLeft is a negative value
         if  (secondsLeft < 0) {
-            console.log('clear setInterval');
-            clearInterval(countdown);
 
+            // bg gif
+            document.querySelector('.timer').style.backgroundImage = `url(${gifUrl.back})`
+
+            // remove return info
+            endTimeDisplayer.textContent = '';
+
+            clearInterval(countdown);
             return
         }
 
@@ -76,8 +98,38 @@ function displayEndTime(timestamp) {
     // Get values using date() object getters
     const hours = endTime.getHours();
     const minutes = endTime.getMinutes();
-    const seconds = endTime.getSeconds();
 
     // Show values in DOM
-    endTimeDisplayer.textContent = `${formatVal(hours)}:${formatVal(minutes)}:${formatVal(seconds)}`;
+    endTimeDisplayer.textContent = `I shall return at ${formatVal(hours)}:${formatVal(minutes)}`;
 }
+
+/**
+ * Start timer when clicking in button
+ */
+function startTimer () {
+    const seconds = parseInt(this.dataset.time);
+
+    // Start timer
+    timer(seconds);
+}
+
+/**
+ * Add event to minutes input
+ * Selecting element from "name" tag
+ */
+document.customForm.addEventListener('submit', function(e) {
+    // Prevent form submit
+    e.preventDefault();
+
+    // Start timer with value in seconds
+    timer(this.minutes.value * 60);
+
+    // Reset input
+    this.reset();
+});
+
+/**
+ * Add click event to all btns
+ */
+buttons.forEach(btn => btn.addEventListener('click', startTimer)); // forEach works in node list
+// [...buttons].map(btn => btn.addEventListener('click', startTimer)); // map doesn't work in nodeList but we can use ... to make it an array
