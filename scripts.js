c4e557e96a29ce1b9f26a7cb87390b382a6639a4
Gonçalo Ramalho <goncaloramalho@Goncalos-iMac-New.local>
2018-08-23 12:31:47 +0200

    Display time left and end time in DOM

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
 // Define countdown var so we can store a setInterval and later clear it
 let countdown;
 
+// Save DOM element where will display values
+const timeLeftDisplayer = document.querySelector('.display__time-left');
+const endTimeDisplayer = document.querySelector('.display__end-time');
+
+/**
+ *  Format values to use extra '0' if value < 10
+ * @param val
+ * @returns {string}
+ */
+const formatVal = (val) => val < 10 ? `0${val}` : val;
+
 /**
  * Run timer
  * @param seconds
@@ -10,8 +21,11 @@ function timer(seconds) {
     const now = Date.now(); // Get current time
     const after = now + seconds * 1000; // Get time and 1s now
 
-    // Show seconds
-    displaySeconds(seconds);
+    // Show time left
+    displayTimeLeft(seconds);
+
+    // Show whe timer should end
+    displayEndTime(after);
 
     // Define setInterval to run every second
     countdown = setInterval(() => {
@@ -26,19 +40,44 @@ function timer(seconds) {
             return
         }
 
-        // Show seconds
-        displaySeconds(secondsLeft);
+        // Show time left
+        displayTimeLeft(secondsLeft);
 
     }, 1000);
 }
 
 /**
- * Show numbers
+ * Display time left (hh:mm:ss)
  * @param seconds
  */
-function displaySeconds(seconds) {
+function displayTimeLeft(seconds) {
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor(seconds / 60);
-    const remainerSeconds = seconds % 60;
-    console.log({hours, minutes, remainerSeconds});
+    const remainingSeconds = seconds % 60;
+
+    const displayTime = `${formatVal(hours)}:${formatVal(minutes)}:${formatVal(remainingSeconds)}`;
+
+    // Show values in document title
+    document.title = displayTime;
+
+    // Show values in DOM
+    timeLeftDisplayer.textContent = displayTime;
+}
+
+/**
+ * Display time when counter should stop
+ * @param seconds
+ */
+function displayEndTime(timestamp) {
+
+    // Get date from timestamp
+    const endTime = new Date(timestamp);
+
+    // Get values using date() object getters
+    const hours = endTime.getHours();
+    const minutes = endTime.getMinutes();
+    const seconds = endTime.getSeconds();
+
+    // Show values in DOM
+    endTimeDisplayer.textContent = `${formatVal(hours)}:${formatVal(minutes)}:${formatVal(seconds)}`;
 }
