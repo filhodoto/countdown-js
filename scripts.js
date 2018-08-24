67cb3d99dd394cb3a4f1fb0c6ca13aa7b1b17f08
Gonçalo Ramalho <goncaloramalho@Goncalos-iMac-New.local>
2018-08-24 10:44:37 +0200

    Add Stop and Play functionality

--- scripts.js
+++ scripts.js
@@ -1,20 +1,42 @@
-// Define countdown var so we can store a setInterval and later clear it
+/**
+ * Define countdown var so we can store a setInterval and later clear it
+ */
 let countdown;
 
-// Save DOM elementsvalues
+/**
+ * Save DOM elements
+ */
 const timeLeftDisplayer = document.querySelector('.display__time-left');
 const endTimeDisplayer = document.querySelector('.display__end-time');
 
-// Get all buttons
-const buttons = document.querySelectorAll('.timer__button');
+// Get timers timeButtons
+const timeButtons = document.querySelectorAll('.timer__button[data-time]');
 
-const gifUrl = {
-    beBack: 'https://media.giphy.com/media/JDKxRN0Bvmm2c/giphy.gif',
-    back: 'https://media.giphy.com/media/yj5UdA4elp8Wc/giphy.gif'
+// Save all action timeButtons in object
+const actionsButtons = {
+    start: document.querySelector('.timer__button.-start'),
+    stop: document.querySelector('.timer__button.-stop')
+};
+
+const bgImg = {
+    run: 'https://media.giphy.com/media/JDKxRN0Bvmm2c/giphy.gif',
+    finished: 'https://media.giphy.com/media/yj5UdA4elp8Wc/giphy.gif',
+    stop: 'https://media.giphy.com/media/3BL45MJDZABqg/giphy.gif'
+};
+
+
+/**
+ * Variable to know when timer is running or not
+ * @param val
+ * @returns {string}
+ */
+let timerState = {
+    runTimer: false, // define if countdown should be running or not
+    secondsLeftMs: 0 // store seconds in miliseconds
 };
 
 /**
- *  Format values to use extra '0' if value < 10
+ * Format values to use extra '0' if value < 10
  * @param val
  * @returns {string}
  */
@@ -26,6 +48,12 @@ const formatVal = (val) => val < 10 ? `0${val}` : val;
  */
 function timer(seconds) {
 
+    // Enable stop button
+    actionsButtons.stop.disabled = false;
+
+    // Disable play button
+    actionsButtons.start.disabled = true;
+
     // Clear previous timer
     clearInterval(countdown);
 
@@ -36,7 +64,7 @@ function timer(seconds) {
     displayTimeLeft(seconds);
 
     // change gif url and show it
-    document.querySelector('.timer').style.backgroundImage = `url("${gifUrl.beBack}")`;
+    document.querySelector('.timer').style.backgroundImage = `url("${bgImg.run}")`;
 
     // Add overlay to display
     document.querySelector('.display').classList.add('-overlay');
@@ -44,28 +72,36 @@ function timer(seconds) {
     // Show whe timer should end
     displayEndTime(after);
 
-    // Define setInterval to run every second
-    countdown = setInterval(() => {
-        // Get ao many seconds are left in seconds (not ms)
-        const secondsLeft = Math.round((after - Date.now()) / 1000);
+    // If runtimer should be running
+    if (timerState.runTimer) {
 
-        // Stop interval if secondsLeft is a negative value
-        if  (secondsLeft < 0) {
+        // Define setInterval to run every second
+        countdown = setInterval(() => {
 
-            // bg gif
-            document.querySelector('.timer').style.backgroundImage = `url(${gifUrl.back})`
+            // Save seconds in ms in our state object so we can restart the counter from this point if it was paused
+            timerState.secondsLeft =  Math.round((after - Date.now()) / 1000);
 
-            // remove return info
-            endTimeDisplayer.textContent = '';
+            // Stop interval if secondsLeft is a negative value
+            if (timerState.secondsLeft < 0) {
 
-            clearInterval(countdown);
-            return
-        }
+                // bg gif
+                document.querySelector('.timer').style.backgroundImage = `url(${bgImg.finished})`;
 
-        // Show time left
-        displayTimeLeft(secondsLeft);
+                // remove return info
+                endTimeDisplayer.textContent = '';
 
-    }, 1000);
+                clearInterval(countdown);
+                return
+            }
+
+            // Show time left
+            displayTimeLeft(timerState.secondsLeft);
+
+        }, 1000);
+
+    }
+    ;
+
 }
 
 /**
@@ -106,20 +142,48 @@ function displayEndTime(timestamp) {
 /**
  * Start timer when clicking in button
  */
-function startTimer () {
+function startTimer() {
     const seconds = parseInt(this.dataset.time);
+
+    // Enable countdown
+    timerState.runTimer = true;
 
     // Start timer
     timer(seconds);
 }
 
 /**
+ * Start timer when clicking in button
+ */
+function stopTimer() {
+
+    // Disable countdown
+    timerState.runTimer = false;
+
+    // change gif url and show it
+    document.querySelector('.timer').style.backgroundImage = `url("${bgImg.stop}")`;
+
+    // Disable stop button
+    actionsButtons.stop.disabled = true;
+
+    // Enable play button
+    actionsButtons.start.disabled = false;
+
+    // Stop interval count
+    clearInterval(countdown);
+
+}
+
+/**
  * Add event to minutes input
  * Selecting element from "name" tag
  */
-document.customForm.addEventListener('submit', function(e) {
+document.customForm.addEventListener('submit', function (e) {
     // Prevent form submit
     e.preventDefault();
+
+    // Enable countdown
+    timerState.runTimer = true;
 
     // Start timer with value in seconds
     timer(this.minutes.value * 60);
@@ -129,7 +193,29 @@ document.customForm.addEventListener('submit', function(e) {
 });
 
 /**
+ * Add evennt listener to STOP btn
+ */
+actionsButtons.stop.addEventListener('click', stopTimer);
+
+
+/**
+ * Add evennt listener to START btn
+ */
+actionsButtons.start.addEventListener('click', function () {
+
+    // Enable countdown
+    timerState.runTimer = true;
+
+    // Enable stop button
+    actionsButtons.stop.disabled = false;
+
+    // Restart timer from time we paused it
+    timer(Number(timerState.secondsLeft));
+
+});
+
+/**
  * Add click event to all btns
  */
-buttons.forEach(btn => btn.addEventListener('click', startTimer)); // forEach works in node list
-// [...buttons].map(btn => btn.addEventListener('click', startTimer)); // map doesn't work in nodeList but we can use ... to make it an array
+timeButtons.forEach(btn => btn.addEventListener('click', startTimer)); // forEach works in node list
+// [...timeButtons].map(btn => btn.addEventListener('click', startTimer)); // map doesn't work in nodeList but we can use ... to make it an array
